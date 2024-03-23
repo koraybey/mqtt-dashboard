14aaa57cddb0cb52f7755850d29d4bc5b154e5cf
koraybey <21336342+koraybey@users.noreply.github.com>
2024-03-23 19:09:05 +0100

    fix(application): change video player dep

--- application/src/components/other/Video.tsx
+++ application/src/components/other/Video.tsx
@@ -1,33 +1,17 @@
-import Hls from 'hls.js'
-import { useEffect, useRef } from 'react'
+import ReactPlayer from 'react-player/lazy'
 
-const hls = new Hls()
-
-export const Video: React.FC = () => {
-    const videoReference = useRef<HTMLVideoElement>(null)
-
-    useEffect(() => {
-        if (Hls.isSupported()) {
-            hls.loadSource(
-                'http://10.147.17.93:8083/stream/home/channel/0/hls/live/index.m3u8'
-            )
-            if (!videoReference.current) return
-            hls.attachMedia(videoReference.current)
-            // hls.on(Hls.Events.ERROR, (err) => {
-            //     console.log(err)
-            // })
-        }
-    }, [videoReference])
-
-    return (
-        <video
-            autoPlay
+export const Video = () => (
+    <div className={'w-full h-full border rounded-lg dark:bg-zinc-950 overf'}>
+        <ReactPlayer
+            playing
+            stopOnUnmount
             muted
-            playsInline
-            ref={videoReference}
-            className={
-                'w-full h-full border rounded-lg dark:bg-zinc-950 object-cover'
+            width={'100%'}
+            height={'100%'}
+            playsinline
+            url={
+                'http://10.147.17.93:8083/stream/home/channel/0/hls/live/index.m3u8'
             }
-        ></video>
-    )
-}
+        />
+    </div>
+)
--- application/vite.config.ts
+++ application/vite.config.ts
@@ -1,11 +1,13 @@
 import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
+import type { PluginOption } from 'vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), visualizer() as PluginOption],
     resolve: {
         // eslint-disable-next-line unicorn/prefer-module
         alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
